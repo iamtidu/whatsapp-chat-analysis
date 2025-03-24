e8d708a427fc2b4c077573b03ca18e72418eb6b2
UDIT KUMAR <117339836+iamtidu@users.noreply.github.com>
2025-03-25 00:36:38 +0530

    Update preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -2,24 +2,25 @@ import re
 import pandas as pd
 
 def preprocess(data):
-    pattern = '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
+    pattern = r'\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
 
     messages = re.split(pattern, data)[1:]
     dates = re.findall(pattern, data)
 
     df = pd.DataFrame({'user_message': messages, 'message_date': dates})
-    # convert message_date type
-    df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%Y, %H:%M - ')
-
+    
+    # Convert message_date to datetime
+    df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%Y, %H:%M - ', errors='coerce')
     df.rename(columns={'message_date': 'date'}, inplace=True)
 
     users = []
     messages = []
+    
     for message in df['user_message']:
-        entry = re.split('([\w\W]+?):\s', message)
-        if entry[1:]:  # user name
+        entry = re.split(r'([\w\W]+?):\s', message, maxsplit=1)
+        if len(entry) > 2:
             users.append(entry[1])
-            messages.append(" ".join(entry[2:]))
+            messages.append(entry[2])
         else:
             users.append('group_notification')
             messages.append(entry[0])
@@ -37,15 +38,7 @@ def preprocess(data):
     df['hour'] = df['date'].dt.hour
     df['minute'] = df['date'].dt.minute
 
-    period = []
-    for hour in df[['day_name', 'hour']]['hour']:
-        if hour == 23:
-            period.append(str(hour) + "-" + str('00'))
-        elif hour == 0:
-            period.append(str('00') + "-" + str(hour + 1))
-        else:
-            period.append(str(hour) + "-" + str(hour + 1))
-
-    df['period'] = period
+    # Creating 'period' column
+    df['period'] = df['hour'].apply(lambda h: f"{h}-{'00' if h == 23 else h+1}")
 
     return df
